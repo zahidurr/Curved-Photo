e4b9791906ea64fb293adcb2bc6966cc88e9dd74
Zahid <zahidur.r@outlook.com>
2023-10-24 16:41:27 +0600

    Initial Commit

---
+++ Curved Photo/Curved_PhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curved_PhotoApp.swift
+//  Curved Photo
+//
+//  Created by Z on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Curved_PhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
