6242f22f498bbc50948a720e2f99413b30e27e40
Zahid <zahidur.r@outlook.com>
2023-10-26 18:34:10 +0600

    Add button

--- Curved Photo/ContentView.swift
+++ Curved Photo/ContentView.swift
@@ -2,20 +2,18 @@
 //  ContentView.swift
 //  Curved Photo
 //
-//  Created by Z on 24/10/23.
+//  Created by Zahid on 24/10/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
         }
-        .padding()
+
     }
 }
 
